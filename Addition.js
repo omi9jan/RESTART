31eeeaf798bf74708f1b7a2fa156aea474d0a7d2
Omkareshwari Khupse <107460519+omi9jan@users.noreply.github.com>
2024-04-23 22:21:36 +0530

    Added create Addition function assignment

---
+++ Addition.js
@@ -0,0 +1,13 @@
+//Create function for addition of two numbers
+   
+    function Add(num1, num2)
+    {
+        let sum = 0;
+        sum = num1 + num2;
+        return sum;
+    }
+   
+    let a = 5;
+    let b = 7;
+    console.log("Addition of two numbers is ",  Add(a, b));
+   
