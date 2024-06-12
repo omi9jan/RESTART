77983db375616c44f38cae6dbe1ae3430297c25b
Omkareshwari Khupse <107460519+omi9jan@users.noreply.github.com>
2024-06-12 23:55:02 +0530

    Added RelationalOperator js assignment

---
+++ RelationalOperator.js
@@ -0,0 +1,92 @@
+let total_bill = 800;
+    if(total_bill >= 1000)
+    {
+        console.log('Person will get 20% discount on Shopping');
+    }
+    else if(total_bill >= 500)
+    {
+        console.log('Person will get 10% discount on Shopping');
+    }
+    else
+    {
+        console.log('Person is not eligible for discount');
+    }
+ 
+//output:
+// Person will get 10% discount on Shopping
+ 
+ 
+ 
+ 
+    let choice = 6;
+    switch(choice){
+        case 1: console.log("1. I Like Listening Songs");
+        case 2: console.log("2. I like playing Mobile Games");
+        case 3: console.log("3. I Like playing Kho-Kho");
+        case 4: console.log("4. I like playing Bat-Minton");
+        case 5: console.log("5. I like Playing Lagori");
+        case 6: console.log("6. I like cooking new dishes");
+        case 7: console.log("7. I like dancing when my mood is not good ");
+        case 8: console.log("8. I like coding tooo much");
+        case 9: console.log("9. I like to keep everone happy");
+        case 10: console.log("10. I like to do meditation");
+    }
+ 
+//output:
+// 6. I like cooking new dishes
+// 7. I like dancing when my mood is not good
+// 8. I like coding tooo much
+// 9. I like to keep everone happy
+// 10. I like to do meditation
+ 
+ 
+let choice = 6;
+    switch(choice){
+        case 1: console.log("1. I Like Listening Songs");
+        break;
+        case 2: console.log("2. I like playing Mobile Games");
+        break;
+        case 3: console.log("3. I Like playing Kho-Kho");
+        break;
+        case 4: console.log("4. I like playing Bat-Minton");
+        break;
+        case 5: console.log("5. I like Playing Lagori");
+        break;
+        case 6: console.log("6. I like cooking new dishes");
+        break;
+        case 7: console.log("7. I like dancing when my mood is not good ");
+        break;
+        case 8: console.log("8. I like coding tooo much");
+        break;
+        case 9: console.log("9. I like to keep everone happy");
+        break;
+        case 10: console.log("10. I like to do meditation");
+    }
+//output:
+// 6. I like cooking new dishes
+ 
+ 
+let a=3;
+    let y=a++;
+    console.log("a is :",a);
+    console.log("y is :",y);
+    console.log("a is :",a);
+//output:
+// a is : 4
+// y is : 3
+// a is : 4
+ 
+ 
+let b=58;
+    let z=b--;
+    console.log("b is :",b);
+    console.log("z is :",z);
+    console.log("b is :",b);
+    console.log("z is :",z);
+ 
+    // b is : 57
+    // z is : 58
+    // b is : 57
+    // z is : 58
+ 
+   
